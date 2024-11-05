a3c8cad902b31190716e2d4dfb886d7c57b60e0f
阿战 <xigchn@163.com>
2024-11-05 20:44:13 +0800

    添加兆能M2preset script

---
+++ scripts/preset-script-ZN_M2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p files/usr/bin/AdGuardHome
+
+AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
+
+wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
+
+chmod +x files/usr/bin/AdGuardHome/AdGuardHome
+
+# Set default theme to luci-theme-argon
+uci set luci.main.mediaurlbase='/luci-static/argon'
+uci commit luci
+exit 0
