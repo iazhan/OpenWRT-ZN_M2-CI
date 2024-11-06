08b95269e69e0e9acf73bed2177a8f744ec3af2e
阿战 <xigchn@163.com>
2024-11-06 19:15:47 +0800

    更换AdGuardHome二进制文件的下载方式

--- scripts/preset-script-ZN_M2.sh
+++ scripts/preset-script-ZN_M2.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# 将 files/usr/bin/AdGuardHome 重命名为 AdGuardHome1
+mv files/usr/bin/AdGuardHome files/usr/bin/AdGuardHome1
+
+# 在 files/usr/bin 目录下创建 AdGuardHome 文件夹
 mkdir -p files/usr/bin/AdGuardHome
 
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
+# 将 AdGuardHome1 文件移动到新建的 AdGuardHome 文件夹中
+mv files/usr/bin/AdGuardHome1 files/usr/bin/AdGuardHome/
 
-wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
+# 将文件名改回 AdGuardHome
+mv files/usr/bin/AdGuardHome/AdGuardHome1 files/usr/bin/AdGuardHome/AdGuardHome
 
 chmod +x files/usr/bin/AdGuardHome/AdGuardHome
 
